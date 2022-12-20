1261cd04e6cf8c3e3189f11c431e3f8c4d553beb
hernanAcevedoM <118073250+hernanAcevedoM@users.noreply.github.com>
2022-12-20 20:25:01 -0300

    Nueva actividad Validar usuario

--- app/src/main/java/com/example/miseventos/ValidarUsuario.java
+++ app/src/main/java/com/example/miseventos/ValidarUsuario.java
@@ -3,12 +3,37 @@ package com.example.miseventos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ValidarUsuario extends AppCompatActivity {
+    private EditText etIngUsuario;
+    private Button btnVolver, btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validar_usuario);
+
+        referencias();
+    }
+
+    //region Eventos y referencias
+    private void eventos(){
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+    private void referencias() {
+
+        etIngUsuario = findViewById(R.id.etIngUsuario);
+        btnVolver = findViewById(R.id.btnVolver);
+        btnEnviar = findViewById(R.id.btnGuardar);
+
     }
 }
