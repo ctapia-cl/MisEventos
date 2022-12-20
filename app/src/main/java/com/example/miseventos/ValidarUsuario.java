c0a0454d4c6847cf7f9c390c72bca2d1593ad5f7
Cristian Tapia <tapiarodriguez@gmail.com>
2022-12-20 20:17:53 -0300

    Se agrega actividdad para identificar usuario

---
+++ app/src/main/java/com/example/miseventos/ValidarUsuario.java
@@ -0,0 +1,14 @@
+package com.example.miseventos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ValidarUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validar_usuario);
+    }
+}
